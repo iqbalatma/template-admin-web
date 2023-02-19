6b9a1683c62ba9f53dd7e76d64a2f20e03ee4d98
iqbalatma <iqbalatma@gmail.com>
2023-02-20 03:23:47 +0700

    finish role controller test on edit method

--- tests/Feature/Managements/RoleControllerTest.php
+++ tests/Feature/Managements/RoleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Managements;
 
+use App\Models\Role;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -30,5 +31,33 @@ class RoleControllerTest extends TestCase
         $this->login(null);
         $response = $this->get(route("roles.index"));
         $response->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+    }
+
+
+    public function testEdit()
+    {
+        $this->login();
+        $role = $this->getDummyRole();
+        $response = $this->get(route("roles.edit", $role->id));
+        $response->assertStatus(JsonResponse::HTTP_OK);
+    }
+    public function testEditUnauthenticated()
+    {
+        $role = $this->getDummyRole();
+        $response = $this->get(route("roles.edit", $role->id));
+        $response->assertStatus(JsonResponse::HTTP_FOUND);
+    }
+    public function testEditUnauthorized()
+    {
+        $this->login(null);
+        $role = $this->getDummyRole();
+        $response = $this->get(route("roles.edit", $role->id));
+        $response->assertStatus(JsonResponse::HTTP_FORBIDDEN);
+    }
+    private function getDummyRole()
+    {
+        return Role::create([
+            "name" => fake()->name()
+        ]);
     }
 }
---
+++ tests/Unit/Managements/RoleServiceTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Unit\Managements;
+
+use Tests\TestCase;
+
+class RoleServiceTest extends TestCase
+{
+}
