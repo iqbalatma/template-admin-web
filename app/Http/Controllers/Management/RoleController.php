f6ad6a1be6fc5b892c3557e100785526ceb93f83
iqbalatma <iqbalatma@gmail.com>
2023-12-11 18:57:35 +0700

    change pulse threshold

--- app/Http/Controllers/Management/RoleController.php
+++ app/Http/Controllers/Management/RoleController.php
@@ -54,6 +54,6 @@ class RoleController extends Controller
 
         if ($this->isError($response)) return $this->getErrorResponse();
 
-        return redirect()->route("roles.index")->with("success", ucfirst(trans("managements/roles.messages.updateSuccess")));
+        return redirect()->route("management.roles.index")->with("success", ucfirst(trans("managements/roles.messages.updateSuccess")));
     }
 }
