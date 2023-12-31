3bc1943a6beffe7d77896520bd865edc1bb5ed6b
iqbalatma <iqbalatma@gmail.com>
2023-12-31 16:17:27 +0700

    add middleware on auth  group route

--- routes/Auth/AuthRoute.php
+++ routes/Auth/AuthRoute.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\Auth\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::name("auth.")->controller(AuthController::class)->group(function (){
-    Route::get("/login", 'login')->name('login');
-    Route::post("/authenticate", 'authenticate')->name('authenticate');
-    Route::post("/logout", 'logout')->name('logout');
+Route::name("auth.")->controller(AuthController::class)->group(function () {
+    Route::get("/login", 'login')->name('login')->middleware("guest");
+    Route::post("/authenticate", 'authenticate')->name('authenticate')->middleware("guest");
+    Route::post("/logout", 'logout')->name('logout')->middleware("auth");
 });
